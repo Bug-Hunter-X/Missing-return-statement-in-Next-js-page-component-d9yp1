04926f4673af36d0db497dfeb52edaa86003946f
BugHunter-X <askzenai@gmail.com>
2024-12-11 11:56:06 +0100

    Add aboutSolution.js file

---
+++ aboutSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/aboutSolution.js
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+export default About;
+```
